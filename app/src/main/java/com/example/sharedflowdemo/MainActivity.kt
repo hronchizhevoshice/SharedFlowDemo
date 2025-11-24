2eb7c5e10a846caf3281d6da9d01af372079c5d3
hronchizhevoshice <hronchizhevoshice@gmail.com>
2025-11-24 14:01:58 +0700

    Collect in background mode

--- app/src/main/java/com/example/sharedflowdemo/MainActivity.kt
+++ app/src/main/java/com/example/sharedflowdemo/MainActivity.kt
@@ -22,6 +22,8 @@ import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.sharedflowdemo.ui.theme.SharedFlowDemoTheme
 import kotlinx.coroutines.flow.SharedFlow
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.repeatOnLifecycle
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,8 +56,11 @@ fun MainScreen(
     val messages = remember { mutableStateListOf<Int>()}
     val lifecycleOwner = LocalLifecycleOwner.current
     LaunchedEffect(key1 = Unit) {
-        sharedFlow.collect {
-            messages.add(it)
+        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            sharedFlow.collect {
+                println("Collecting $it")
+                messages.add(it)
+            }
         }
     }
 
